0420efe1b6ea6f482593ef10c946c26dfccbe51b
JosiaSalgado <josias.salgado.f@gmail.com>
2023-10-10 08:42:50 -0300

    applying .js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors'
-import router from './routes/dashboard.routes'
+import router from './routes/dashboard.routes.js'
 
 const app = express();
 

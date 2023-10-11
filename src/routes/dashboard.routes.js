8fbd69407402e8eda8c4172139d3dd3c8df96f5f
Felipe Reichert <darighan@github.com>
2023-10-11 14:14:54 -0300

    Fixing route for carriers

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getAllSales, getAllClients, getAllProviders } from "../controllers/dashboard.controller.js";
+import { getAllSales, getAllClients, getAllProviders, getAllCarriers } from "../controllers/dashboard.controller.js";
 
 const router = Router();
 
 router.get('/salesGross', getAllSales)
 .get('/clients', getAllClients)
 .get('/providers', getAllProviders)
-.get('/carriers', getAllProviders)
+.get('/carriers', getAllCarriers)
 
 export default router;

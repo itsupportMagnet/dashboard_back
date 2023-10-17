b006fa1856049d9a92f395cd85a61be93d54cddc
Felipe Reichert <darighan@github.com>
2023-10-17 15:14:20 -0300

    fix cors call header

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 
 app.use((req,res,next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', webLink || '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST , PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next();

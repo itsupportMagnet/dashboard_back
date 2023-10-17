8e7aa760c05b6ea943b7ad4ea0ae24518b1f8fa2
Felipe Reichert <darighan@github.com>
2023-10-17 09:27:48 -0300

    Testing Cors Fix

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ app.use(cors({
   optionsSuccessStatus: 200
 }));
 
+app.use((req,res,next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET, POST , PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next();
+  
+});
 
 app.use(express.json())
 app.use(router);

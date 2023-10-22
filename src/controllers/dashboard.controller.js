c8abc380ed48436db3ad533500c4258d36fe2144
JosiasSalgadoBetterCommerce <josias.salgado@bettercommerce.cl>
2023-10-21 19:02:07 -0700

    testing

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1141,11 +1141,12 @@ export const getOperations = async (req, res) => {
 
 export const changeStatus = async (req, res) => {
   const {idOperation, status} = req.body;
-  
-  changeOperationStatus(idOperation, status)
-  .then(()=> res.status(500).json({message: 'ok'}))
-  .catch(error => {
-    console.log(error);
-    res.status(500).json({ error })
-  })
+
+  console.log(idOperation, status);
+  // changeOperationStatus(idOperation, status)
+  // .then(()=> res.status(500).json({message: 'ok'}))
+  // .catch(error => {
+  //   console.log(error);
+  //   res.status(500).json({ error })
+  // })
 }

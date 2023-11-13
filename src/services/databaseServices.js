b7beb49e7eca429d15478accfcbdb35fb5f0e152
Felipe Reichert <darighan@github.com>
2023-11-13 16:05:10 -0300

    Fixing Endpoint error

--- src/services/databaseServices.js
+++ src/services/databaseServices.js
@@ -47,6 +47,17 @@ export const saveQuoteSent = async (quoteID, modeOfOperation, pol, deliveryAddre
     });
 }
 
+export const  getCarrierFeeByQuoteId = async (id) => {
+  const query = 'SELECT * FROM carriers_fees WHERE quoteID = ?';
+  try {
+    const [rows] = await pool.query(query, [id]);
+    return [rows]
+  } catch (error) {
+    console.error("Error to get specific quote:", error);
+    throw error;
+  }
+}
+
 export const getQuoteFeeById = async (id) => {
   const query = 'SELECT * FROM carriers_fees WHERE quoteID = ?';
   try {

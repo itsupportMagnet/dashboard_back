8cb32b394d0aa87c716a3bc23fa88676abf90202
danielMagnetlogistic <daniel.magnet@hotmail.com>
2023-11-28 09:42:07 -0300

    add new middleware

---
+++ middlewares/verifyRol.js
@@ -0,0 +1,23 @@
+import { pool } from "../db.js";
+
+export const validateRole = async (req, res, next) => {
+  //traer la info del cliente mediante la base de datos
+  //verificar si el rol(localStorage) es igual al de la bd
+  //v- puede seguir con la acción
+  //f- tirara error
+  const { email } = req.body;
+  !email || res.status(500).json({ message: "email isn't exits or appear" });
+
+  //consulta a la bd
+  query = "SELECT `rol` FROM users WHERE email = ?";
+  pool
+    .query(query, [email])
+    .then((rows) => {
+      user = rows[0]
+
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json(error)
+    });
+};

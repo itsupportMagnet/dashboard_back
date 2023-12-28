387da526f9f9979a6ec2210688e2482821e6c031
JosiasSalgadoBetterCommerce <josias.salgado@bettercommerce.cl>
2023-12-28 16:55:18 -0700

    returning an object

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1680,7 +1680,7 @@ export const newSummarySalesGrossById = async (req, res) => {
 
 export const getSalesGrossById = async (req, res) => {
   fetchSaleGrossInfoById(req.params.id)
-  .then(data => res.status(200).json(data))
+  .then(data => res.status(200).json(data[0]))
   .catch(error => {
     console.log(error);
     res.status(500).json({ error })

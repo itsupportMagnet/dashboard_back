3db326f1be7803abfb767301e28b82bcfdff619f
FelipeReichert <felipe.reichertgodoy@gmail.com>
2024-02-26 10:36:44 -0300

    Refactor: Changing somethjing

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ import { createPool } from "mysql2/promise";
 const {dbHost, dbUser, dbPassword, dbDataBase, dbPort} = process.env
 
 export const pool = createPool({
-	host: 'tms-database-do-user-15719841-0.c.db.ondigitalocean.com',
-	user: 'doadmin',
-	password: 'AVNS_MDpFBK2FccFXVeXfw5r',
-	port: '25060',
-	database: 'develop'
+	host: dbHost,
+	user: dbUser,
+	password: dbPassword,
+	port: dbPort,
+	database: dbDataBase
 })

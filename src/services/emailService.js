f7a8087818eaf0f7a0b2169531991dcd5aafac10
JosiasSalgadoBetterCommerce <josias.salgado@bettercommerce.cl>
2024-05-02 16:32:53 -0700

    Feature: Send more than 1 email per time

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -2,26 +2,29 @@ import sgMail from '@sendgrid/mail';
 const { sendGridKey, fromEmail, replyToEmail } = process.env
 const sendGridTestKey = "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";
 
-export const sendEmail = async (carrier, emailSubject, emailBody) => {
-  console.log(carrier);
+export const sendEmail = async (carriers, emailSubject, emailBody) => {
   // sgMail.setApiKey(sendGridKey)//Cambiar a sendGridKey
-  const msg = {
-    to: carrier, // Change to your recipient
-    from: 'andre.gonzalez@easyfreight.ai', // Change to your verified sender
-    replyTo: 'Andre.gonzalez@magnetlogisticscorp.com',
-    subject: emailSubject,
-    // bcc: bccRecipients,
-    text: 'EasyFreight 2024',
-    html: emailBody,
-  }
 
-  sgMail.setApiKey(sendGridTestKey);
+  carriers.forEach(carrierEmail => {
+    const msg = {
+      to: carrierEmail, // Change to your recipient
+      from: 'andre.gonzalez@easyfreight.ai', // Change to your verified sender
+      replyTo: 'Andre.gonzalez@magnetlogisticscorp.com',
+      subject: emailSubject,
+      // bcc: bccRecipients,
+      text: 'EasyFreight 2024',
+      html: emailBody,
+    }
 
-  sgMail.send(msg).then(() => {
+    sgMail.setApiKey(sendGridTestKey);
+
+    sgMail.send(msg).then(() => {
       console.log('Email sent')
     })
-    .catch((error) => {
-      console.error(error)
-    })
+      .catch((error) => {
+        console.error(error)
+      })
+  })
+
 
 };

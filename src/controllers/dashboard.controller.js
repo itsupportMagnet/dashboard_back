444704e2ecb323a6fc925a32b9c8efa55f06f31b
JosiasSalgadoBetterCommerce <josias.salgado@bettercommerce.cl>
2024-05-21 17:36:53 -0700

    Adding console.log

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1313,8 +1313,9 @@ export const addClient = async (req, res) => {
 }
 
 export const addCarrier = async (req, res) => {
-
+  
   const { carrierId, name, contact, mc, dot, SCAC, EIN, form1099, insurance, address, city, zipcode, state, country, doct, carrierType, carrierPhone, carrierEmail, idCompany, ports } = req.body;
+  console.log(carrierId, name, contact, mc, dot, SCAC, EIN, form1099, insurance, address, city, zipcode, state, country, doct, carrierType, carrierPhone, carrierEmail, idCompany, ports);
   // const phonesJSON = JSON.stringify(phoneNumbers);
   // const emailsJSON = JSON.stringify(carrierEmails);
   const addNewCarrierPromise = addNewCarrier(carrierId, name, contact, mc, dot, SCAC, EIN, form1099, insurance, address, city, zipcode, state, country, doct, carrierType, carrierPhone, carrierEmail, idCompany)

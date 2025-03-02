5749f5b3cdd67bf615f41459678a16dd16b1167f
Humberto Dlc <h.j.delascasas@gmail.com>
2025-03-01 22:28:33 -0800

    Logging

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1316,7 +1316,11 @@ export const addClient = async (req, res) => {
 export const addCarrier = async (req, res) => {
   
   const { carrierId, name, contact, mc, dot, SCAC, EIN, form1099, insurance, address, city, zipcode, state, country, doct, carrierType, carrierPhone, carrierEmail, idCompany, ports } = req.body;
+  console.log(req);
   console.log(req.body);
+  console.log(city);
+  console.log(zipcode);
+  console.log(ports);
   const addNewCarrierPromise = addNewCarrier(carrierId, name, contact, mc, dot, SCAC, EIN, form1099, insurance, address, city, zipcode, state, country, doct, carrierType, carrierPhone, carrierEmail, idCompany, ports);
   
   const addNewCarrierPortsPromise = addNewCarrierPorts(carrierEmail, carrierId, ports, idCompany);

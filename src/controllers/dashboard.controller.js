892400a49757404d5668bcb04c73d89d725f1068
Humberto Dlc <h.j.delascasas@gmail.com>
2025-03-05 16:48:32 -0800

    Logger

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1937,7 +1937,7 @@ export const getAllWarehousesData = (req, res) => {
 export const addWarehouse = (req, res) => {
   const { warehouseId, warehouseType, name, contact, phoneNumber, email, country, state, city, zipcode, address, scheduling_preference, idCompany } = req.body;
 
-  console.log(req.body);
+  console.log("Req", req.body);
   addNewWarehouse(warehouseId, warehouseType, name, contact, phoneNumber, email, country, state, city, zipcode, address, scheduling_preference, idCompany)
     .then(() => res.status(200).json({ message: "ok" }))
     .catch(error => {

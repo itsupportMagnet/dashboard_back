00cca66f15119e86ace09e8134228b38ea7e67c1
Anders Rojas Mezarina <67669989+andersrrm@users.noreply.github.com>
2025-04-22 23:02:48 -0500

    feature: added username logic

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,9 @@
 import { pool } from '../../db.js';
-export const createUser = async (fullName, email, password, phone, role) => {
-  const query = 'INSERT INTO users (fullName, email, password, phone, role) VALUES (?, ?, ?, ?, ?)';
+export const createUser = async (fullName, userName, email, password, phone, role) => {
+  const query = 'INSERT INTO users (fullName, userName, email, password, phone, role) VALUES (?, ?, ?, ?, ?, ?)';
   try {
-    const [result] = await pool.execute(query, [fullName, email, password, phone, role]);
-    return { id: result.insertId, fullName, email, password, phone, role };
+    const [result] = await pool.execute(query, [fullName, userName, email, password, phone, role]);
+    return { id: result.insertId, fullName, userName, email, password, phone, role };
   } catch (error) {
     console.error('Error creating user:', error);
     throw new Error('Database error');
